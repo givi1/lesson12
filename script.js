3cccf4a3ad262cbe603b1236e71957b659a03d06
givi1 <givichargeishvili@gmail.com>
2021-07-12 15:57:11 +0400

    create index.html and script.js added some tags

---
+++ script.js
@@ -0,0 +1,65 @@
+let person1 = {
+    firstName:"levani",
+    age:60,
+    profession:"doctor",
+    numberOfDiploma: 3,
+    height: 170,
+	hairColor: 'brown',
+}
+let person2 = {
+    firstName:"giorgi",
+    age:35,
+    profession:"firemen",
+    height: 175,
+	hairColor: 'brown',
+}
+let person3 = {
+    firstName:"saba",
+    age:35,
+    profession:"teacher",
+    numberOfDiploma: 2,
+}
+let person4 = {
+    firstName:"sandro",
+    age:45,
+    profession:"Lawyer",
+    numberOfDiploma: 2,
+}
+let person5 = {
+    firstName:"mamuka",
+    age:25,
+    profession:"Basketball player",
+    height: 198,
+    Numberofcups:5,
+}
+
+let persons = [person1,person2,person3,person4,person5]
+
+let person = {
+    firstName:"gordon", 
+    lastName:"ramsey",
+    profession:"chef",
+    niknemes:[ "perigordon rammsay", "bangordon ramsay ", "vigordon ramsay "," gordon ramsayuri" ],
+    age:55,
+    monthlyIncome:45000000,
+    
+  }
+ let rest ={
+  width:15,
+  height:10,
+ }
+ let Rectanglearea = rest.width*rest.height;
+ console.log( Rectanglearea  )   
+ let object1 ={
+   x:20,
+   y:15,
+  }
+ let object2 ={
+  x:18,
+  y:30,
+}
+ let x = object1.x-object2.x;
+ let y = object1.y-object2.y;
+ console.log(Math.sqrt(x * x + y * y))
+
+ let obcts =[object1,object2]
